657ddbc7c2822df913f650cb50b04420a292cb0f
iamsyahirah <syahirahmohammad89@gmail.com>
2024-10-05 23:50:22 +0800

    feat: middleware

---
+++ food-app/app/Http/Middleware/AbleCreateOrder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AbleCreateOrder
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string ...$guards): Response
+    {
+        $user = auth()->user();
+
+        if ($user->role_id != 1 && $user->role_id != 4) {
+            return response('You cannot access this page.', 403);
+        }
+
+        return $next($request);
+
+    }
+}
