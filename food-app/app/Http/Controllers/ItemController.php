f44e4447d0e9a00108c865cdf7b8d479a06aa227
iamsyahirah <syahirahmohammad89@gmail.com>
2024-10-07 08:45:31 +0800

    feat: API create item

--- food-app/app/Http/Controllers/ItemController.php
+++ food-app/app/Http/Controllers/ItemController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
     public function store(Request $request)
     {
-        return 'oke';
+        $request->validate([
+            'name' => 'required|max:100',
+            'price' => 'required|integer',
+            'image_file' => 'nullable|mimes:jpg,bmp,png',
+        ]);
+
+        $item = Item::create($request->all());
+
+        return response(['data' => $item]);
     }
 }
